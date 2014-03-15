e4bfbc262620614bb1d158ae538d41860e06bc7e
App Academy Student <student@appacademy.io>
2014-03-14 20:29:38 -0400

    Moves nav bar color changes to in between sections

--- javascript.js
+++ javascript.js
@@ -2,15 +2,15 @@ var updateNavBarColors = function () {
   var scrolled = $(window).scrollTop();
 
   $("nav li a").removeClass("active-nav-block");
-  if (scrolled >= 1126 && scrolled < 2467) {
+  if (scrolled >= 1126-450 && scrolled < 2467-450) {
     $("#bio-section-link").addClass("active-nav-block");
-  } else if (scrolled >= 2467 && scrolled < 4038) {
+  } else if (scrolled >= 2467-450 && scrolled < 4038-450) {
     $("#recent-projects-section-link").addClass("active-nav-block");
-  } else if (scrolled >= 4038 && scrolled < 6487) {
+  } else if (scrolled >= 4038-450 && scrolled < 6487-450) {
     $("#experience-section-link").addClass("active-nav-block");
-  } else if (scrolled >= 6487 && scrolled < 7604) {
+  } else if (scrolled >= 6487-450 && scrolled < 7604-450) {
     $("#links-section-link").addClass("active-nav-block");
-  } else if (scrolled >= 7604) {
+  } else if (scrolled >= 7604-450) {
     $("#contact-section-link").addClass("active-nav-block");
   };
 };
